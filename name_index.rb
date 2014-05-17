b1d86c8ddf8356b162bb3adbcd604cd50cac9c72
Tatsuya Ogi <t.oginogin@gmail.com>
2014-05-18 08:14:29 +0900

    first answer

--- name_index.rb
+++ name_index.rb
@@ -1,5 +1,25 @@
 class NameIndex
+
   def self.create_index(names)
-    []
+    patterns = {
+      'ア' => 'ア-オ',
+      'カ' => 'カ-コガ-ゴ',
+      'サ' => 'サ-ソザ-ゾ',
+      'タ' => 'タ-トダ-ド',
+      'ナ' => 'ナ-ノ',
+      'ハ' => 'ハ-ホバ-ボ',
+      'マ' => 'マ-モ',
+      'ヤ' => 'ヤ-ヨ',
+      'ラ' => 'ラ-ロ',
+      'ワ' => 'ワヲン'
+    }
+
+    indexed_names = patterns.each_with_object({}) do |(key, value), indexed|
+      matched_names = names.each_with_object([]) do |name, matched|
+        matched << name if /\A([#{value}])/ =~ name
+      end
+      indexed[key] = matched_names.sort if matched_names.any?
+    end
+    indexed_names.to_a
   end
 end
