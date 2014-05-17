735ab8d81449174a88ba6fa0333179d90db4bfc7
Tatsuya Ogi <t.oginogin@gmail.com>
2014-05-18 08:30:59 +0900

    add test case

--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -2,4 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
   specify { expect(NameIndex.create_index(['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ'])).to eq [ ['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']], ['ハ', ['ハマダ', 'ババ']], ['ワ', ['ワダ']] ] }
+
+  specify { expect(NameIndex.create_index(['ンゴ', 'パパス', 'オギ', 'ジェームズ', 'シェームズ', 'オキ'])).to eq [ ['ア', ['オキ','オギ']], ['サ', ['シェームズ', 'ジェームズ']], ['ハ', ['パパス']], ['ワ', ['ンゴ']] ] }
 end
